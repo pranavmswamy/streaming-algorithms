85df90278c5641fa42907b6fa919661b08f3c9fa
Pranav <pranavmpv@gmail.com>
2020-04-11 12:30:30 -0700

    Changed to global bit array-same array for all 30 streams.

--- task1.py
+++ task1.py
@@ -20,7 +20,7 @@ def myhashs(user_id):
 
     m = 69997  # len of bit array
     hash_result = []
-    num_hashes = 5000 # change in driver() func also if you change it here.
+    num_hashes = 200 # change in driver() func also if you change it here.
     for i in range(1, num_hashes + 1):
         hashed_position = (h1(user_to_int(user_id)) + i * h2(user_to_int(user_id))) % m
         hash_result.append(hashed_position)
@@ -28,7 +28,7 @@ def myhashs(user_id):
     return hash_result
 
 
-def bloom_filter(bit_array, stream_users, num_hashes):
+def bloom_filter(bit_array, stream_users, previous_users, num_hashes):
     """
     :param bit_array: bit array for bloom filter
     :param stream_users: list of user_ids (string).
@@ -36,7 +36,7 @@ def bloom_filter(bit_array, stream_users, num_hashes):
     :param previous_users_count: count of how many users previously seen.
     :return: list storing index of data batch and false positive rate for that batch of data.
     """
-    previous_users = set()
+    # previous_users = set()
     false_positive = 0
     for user in stream_users:
         already_set_count = 0
@@ -60,11 +60,12 @@ def driver():
     bx = BlackBox()
     num_of_asks = 30
     fpr = []
-    num_hashes = 5000 # change in myhashs() func also if you change it here.
+    bit_array = [0 for _ in range(69997)]
+    previous_users = set()
+    num_hashes = 200 # change in myhashs() func also if you change it here.
     for i in range(num_of_asks):
         stream_users = bx.ask("users.txt", 100)
-        bit_array = [0 for _ in range(69997)]
-        batch_fpr = bloom_filter(bit_array, stream_users, num_hashes)
+        batch_fpr = bloom_filter(bit_array, stream_users, previous_users, num_hashes)
         fpr.append((i, batch_fpr))
 
     for f in fpr:
